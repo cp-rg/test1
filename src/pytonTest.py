0cd42e4e2153657007ceb40ff964d8a5cc0ff527
cp-rg <cesarpaul@protonmail.com>
2026-02-06 13:41:53 -0500

    Adding the Wificommunication

---
+++ src/pytonTest.py
@@ -0,0 +1,10 @@
+import requests
+
+ESP32_IP = "http://192.168.86.44/setString"   # Change to your ESP32 IP
+text_to_send = "test"
+
+try:
+    response = requests.post(ESP32_IP, data=text_to_send)
+    print("ESP32 replied:", response.text)
+except Exception as e:
+    print("Error sending request:", e)
