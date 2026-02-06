82061f7fcbbcea61b3b89dc175cb8d50f3cba948
cp-rg <cesarpaul@protonmail.com>
2026-02-06 14:43:07 -0500

    validation 2

--- src/pytonTest.py
+++ src/pytonTest.py
@@ -1,7 +1,7 @@
 import requests
 
 ESP32_IP = "http://192.168.86.44/setString"   # Change to your ESP32 IP
-text_to_send = "redd"
+text_to_send = "dddd"
 
 try:
     response = requests.post(ESP32_IP, data=text_to_send)
